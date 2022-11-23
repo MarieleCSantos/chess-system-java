bd36aaabf3925968ff45f6b94fcbb311a9b08491
Mariele Santos <marielecsantos@outlook.com>
2022-11-23 01:11:14 -0300

    possible moves of a piece

--- src/src/chess/ChessPiece.java
+++ src/src/chess/ChessPiece.java
@@ -5,7 +5,7 @@ import boardgame.Piece;
 import lombok.Getter;
 
 @Getter
-public class ChessPiece extends Piece {
+public abstract class ChessPiece extends Piece {
     private Color color;
 
     public ChessPiece(Board board, Color color) {
