04d12fb7a92eee2d3ba3f4b62c083d9f786d9340
Mariele Santos <marielecsantos@outlook.com>
2023-01-12 09:51:54 -0300

    better treatment to promotion

--- src/src/chess/ChessMatch.java
+++ src/src/chess/ChessMatch.java
@@ -6,7 +6,6 @@ import boardgame.Position;
 import chess.pieces.*;
 import lombok.Getter;
 
-import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -104,7 +103,7 @@ public class ChessMatch {
         }
 
         if (!type.equals("B") && !type.equals("N") && !type.equals("R") && !type.equals("Q")) {
-            throw new InvalidParameterException("Invalid type for promotion.");
+            return promoted;
         }
 
         Position pos = promoted.getChessPosition().toPosition();
